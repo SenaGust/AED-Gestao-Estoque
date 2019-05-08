acff044e4582788581f1eeedf5d5c5496aaabfd6
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2019-05-08 15:17:32 -0300

    Teste

--- Gestão de Estoque - Produtos/Program.cs
+++ Gestão de Estoque - Produtos/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gestão_de_Estoque___Produtos
+namespace Gestão_de_Estoque___Produtos   //teste
 {
     class Program
     {
