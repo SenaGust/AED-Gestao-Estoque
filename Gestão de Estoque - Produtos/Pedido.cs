3eb2d1e9a6424f66afcc18a147afbcfb17656ef2
João Soares <soares.jvm@gmail.com>
2019-05-08 16:14:57 -0300

    Classe Pedido: Construtor, Equals, CompareTo

--- Gestão de Estoque - Produtos/Pedido.cs
+++ Gestão de Estoque - Produtos/Pedido.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Gestão_de_Estoque___Produtos
 {
-    class Pedido:IDado   //não vai ter relação nenhuma com a árvore 
+    class Pedido : IDado   //não vai ter relação nenhuma com a árvore 
     {
         #region Atributos
         public int Id { get; set; }
@@ -14,39 +14,41 @@ namespace Gestão_de_Estoque___Produtos
         #endregion
 
         #region Construtor
-        public Pedido(int Id)
+        public Pedido(int Id, int QuantidadeProdutos)
         {
-            
+            this.Id = Id;
+            this.QuantidadeProdutos = QuantidadeProdutos;
         }
         #endregion
 
         #region Métodos
-        
+
         #endregion
 
         #region Métodos Interface
-        public int CompareTo(object obj)
+        public int CompareTo(object obj) // Compara os pedidos usando o Id
         {
-            Pedido item = (Pedido)(obj);
+            Pedido aux = (Pedido)(obj);        
 
-            //Quando é menor, maior e igual
-
-            return 0;
+            if (Id < aux.Id)
+                return -1;
+            else if (Id > aux.Id)
+                return 1;
+            else
+                return 0;            
         }
         public override bool Equals(object obj)
         {
-            Pedido item = (Pedido)(obj);
+            Pedido aux = (Pedido)(obj);
 
-            //Quando é igual?
-
-            return false;
+            return Id == aux.Id;            
         }
         public override string ToString()
         {
             StringBuilder auxString = new StringBuilder();
 
             auxString.AppendLine("Pedido nº" + Id);
-            auxString.AppendLine(Itens.ToString());
+            auxString.AppendLine("Quantidade de itens no pedido: " + QuantidadeProdutos);
 
             return auxString.ToString();
         }
